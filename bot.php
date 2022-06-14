05a47258a67a51d8bdffb5ab521bdc97b5fc3fa3
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-06-14 22:46:50 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,79 +1,136 @@
-<?php
+// @T7TT1
+//dev//matthew
+//ch M1_m2s
+<?php 
+
 ob_start();
-define('API_KEY', '5507292652:AAFRuWheBbrrWXmzXnY0obJNhBrfmngLWss');
-echo "https://api.telegram.org/bot" . API_KEY . "/setwebhook?url=" . $_SERVER['SERVER_NAME'] . "" . $_SERVER['SCRIPT_NAME'];
 
-function bot($method, $datas = [])
-  {
-  $url = "https://api.telegram.org/bot" . API_KEY . "/" . $method;
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
-  $res = curl_exec($ch);
-  if (curl_error($ch))
-    {
-    var_dump(curl_error($ch));
+$API_KEY = '5507292652:AAFRuWheBbrrWXmzXnY0obJNhBrfmngLWss';
+define('API_KEY',$API_KEY);
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
     }
-    else
-    {
-    return json_decode($res);
-    }
-  }
-
+}
+##------@M1_m2s-------##
+##------@T7TT1-------## 
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$text = $message->text;
+$chat_id = $message->chat->id;
+$message_id = $message->message_id;
+$user = $update->message->from->username;
+$last_name = $update->message->from->last_name;
+$from_id = $update->message->from->id;
+$message_id = $update->message->message_id;
+$user_id = $update->message->from->user_id;
+$time = time() + (979 * 11 + 1 + 30);
+$username = $message->from->username;
 $update = json_decode(file_get_contents('php://input'));
 $message = $update->message;
 $chat_id = $message->chat->id;
 $text = $message->text;
+//(ماثيوو)
+$admin = "201230149";
+$fwd = $message->forward_from_chat->id;
+$new_member = $update->message->new_chat_member; 
+$left = $update->message->left_chat_member; 
+$textmsg = $message->text;
+$message_id = $message->message_id;
+$rep = $message->reply_to_message; 
+$rep_msg = $rep->message_id; 
+$id_sudo = 201230149;
+$get = file_get_contents('file.txt');
+$ex = explode("\n", $get);
+$count = count($ex);
+$type = $update->message->chat->type;
+$re = $update->message->reply_to_message;
+$re_id = $update->message->reply_to_message->from->id;
+$re_user = $update->message->reply_to_message->from->username;
+$user_id = $update->message->from->id;
+//my ch :- M1_m2s
+$re_msgid = $update->message->reply_to_message->message_id;
+$username = $message->from->username;
 $chat_id2 = $update->callback_query->message->chat->id;
-$message_id = $update->callback_query->message->message_id;
-$data = $update->callback_query->data;
-$from_id = $message->from->id;
+$message_id = $update->message->message_id;
+$id = $message->from->id;
 $name = $update->message->from->first_name;
-$from_id = $message->from->id;
+//dev:-matthew
+if($text == "/start"){
+bot('sendMessage',[
+'chat_id'=>$chat_id, 
+'text'=>"
+🛠- اهــــلا بــــك عزيــــزي في بوت كوكل بلي
 
-$ex = explode(' ', $text);
+🛠- البــــوت الآۆلْ علـّۓ. تلــــكرام
 
-if (preg_match('/\/start .*/', $text))
-  {
-  bot('sendMessage', ['chat_id' => $chat_id, 'text' => "- مرحبا بك ، [$name](tg://user?id=$chat_id)
-- في بوت البحث وتحميل تطبيقات  - ✅ 
-• يقوم البوت بأعطائك مجموعه نتائج للتطبيقات التي تبحث . . .
--قم بارسال اسم التطبيق واختاره وسيرسل البوت لك ملف التطبيق ؛⚜️", 'parse_mode' => "MarkDown", 'disable_web_page_preview' => true, 'reply_markup' => json_encode(['inline_keyboard' => [[['text' => "• اضغط هنا وتابع جديدنا 🇮🇶؛", 'url' =>
-"https://t.me/c3d3d"]], ]]) ]);
+🛠- يمكنك تحــــميل اي برنــــامج
+
+🛠- اكتب بحث+اسم البرنامج
+
+⚠️- كمثال:- بحث هولا 
+
+المطور:- @T7TT1
+قناة المطور:- @M1_M2S
+ 
+ ",
+
+'reply_to_message_id'=>$message->$message_id,
+]);
+}
+//matthew//ماثيو(محمد قيس)
+if(preg_match('/^(بحث) (.*)/s', $text, $stor)){
+$rs = 'https://play.google.com/store/search?q='.urlencode($stor[2]);
+bot('sendChatAction', [
+'chat_id'=>$chat_id,
+'action'=>'typing',]);
+sleep(1);
+bot('sendMessage',[
+'chat_id'=>$chat_id,
+'parse_mode'=>'markdown',
+'disable_web_page_preview'=>true,
+'text'=>"*🎉- اهــــــلا عزيــــــزي
+
+🛠- تـــم العثور علـّۓ. البرنامج بنجااح✅
+〰〰〰〰〰〰〰〰〰〰〰〰
+
+👨¦ اسمك:- $name
+🆔¦ ايديك:- $from_id
+📟¦ معرفك:- $username
+
+*\n\n[💡- حسناا اضغط هناا لتحميل البرنامج✅]($rs)",]);}
+$sudo = 201230149;
+$get = explode("\n", file_get_contents('memberbot.txt'));
+if($text == '/start' and !in_array($chat_id, $get)){
+file_put_contents('memberbot.txt',"\n" . $chat_id, FILE_APPEND);
+}
+if($text == '/users' and $id == $sudo){
+ $count = count($get);
+  bot('sendmessage',[
+    'chat_id'=>$chat_id,
+    'parse_mode'=>'markdown',
+    'text'=>"Your Bot Member : $count",
+  ]);
   }
-
-if ($text != '/start')
-  {
-  if (preg_match('/.*\/.*/', $text))
-    {
-    $dl = explode('<a id="download_link"', file_get_contents('https://apkpure.com/ar/' . $text . '/download?from=details'));
-    $dl = explode('"', $get[1]);
-    $dl = $dl[9];
-    file_put_contents('@ssa_15.apk', file_get_contents($dl));
-    bot('sendDocument', ['chat_id' => $chat_id, 'document' => new CURLFile($dl) ]);
-    }
-    else
-    {
-    $get = explode('<dl class="search-dl">', file_get_contents('https://apkpure.com/ar/search?q=' . urlencode($text)));
-    for ($i = 0; $i < count($get); $i++)
-      {
-      $a = explode('"', $get[$i]);
-      $name = $a[1];
-      $url = $a[5];
-      $res['inline_keyboard'][] = [['text' => $name, 'switch_inline_query' => $url]];
-      }
-
-    bot('sendMessage', ['chat_id' => $chat_id, 'text' => '🔎| من فضلك اختر احد التطبيقات لاقوم بتحميلها : ', 'reply_markup' => json_encode($res) ]);
-    }
-  }
-  bot('answerInlineQuery',[
-            'inline_query_id'=>$update->inline_query->id,    
-            'results' => json_encode([[
-                'type'=>'article',
-                'id'=>base64_encode(rand(5,555)),
-                'title'=>$update->inline_query->query,
-                'description'=>'👁‍🗨 By : @ssa_15',
-             'input_message_content'=>['parse_mode'=>'HTML','message_text'=>$update->inline_query->query],
-          ]])
-        ]);
+//my ch: - M1_m2s
+$bc = explode("/bc", $text);
+if($bc and $id == $sudo){
+for($y=0;$y<count($get); $y++){
+bot('sendMessage', [
+'chat_id'=>$get[$y],
+'text'=>"$bc[1]",
+'parse_mode'=>markdown,
+'disable_web_page_preview'=>true,
+]);
+}
+}
+##------dev/ماثيو//محمد قيس------#
+##------my/ch:-@M1_m2------##
